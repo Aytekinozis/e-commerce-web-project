9821369252c94a2de93e7e3b03d44d78629a4bdd
Aytekinozis <aytekinozis93@gmail.com>
2024-03-14 16:46:56 +0300

    category actions

--- my-app/src/store/actions/globalActions.js
+++ my-app/src/store/actions/globalActions.js
@@ -2,10 +2,12 @@ import { useDispatch } from "react-redux";
 import { API } from "../../api/api";
 import {
   SET_CATEGORIES,
+  SET_CAT_FETCH_STATE,
   SET_LANGUAGE,
   SET_ROLES,
   SET_THEME,
 } from "../reducers/globalReducer";
+import { FETCH_STATES } from "../reducers/productReducer";
 
 export const getRoles = () => (dispatch) => {
   API.get("/roles")
@@ -15,6 +17,20 @@ export const getRoles = () => (dispatch) => {
     })
     .catch((err) => {
       console.log(err);
+    });
+};
+
+export const getCategories = () => (dispatch) => {
+  dispatch(setCategoriesFetchState(FETCH_STATES.FETCHING));
+  API.get("/categories")
+    .then((res) => {
+      //console.log(res.data);
+      dispatch(setCategories(res.data));
+      dispatch(setCategoriesFetchState(FETCH_STATES.FETCHED));
+    })
+    .catch((err) => {
+      console.log(err);
+      dispatch(setCategoriesFetchState(FETCH_STATES.FETCH_FAILED));
     });
 };
 
@@ -29,6 +45,12 @@ export const setCategories = (categories) => {
   return {
     type: SET_CATEGORIES,
     payload: categories,
+  };
+};
+export const setCategoriesFetchState = (catFetchState) => {
+  return {
+    type: SET_CAT_FETCH_STATE,
+    payload: catFetchState,
   };
 };
 
--- my-app/src/store/reducers/globalReducer.js
+++ my-app/src/store/reducers/globalReducer.js
@@ -1,11 +1,15 @@
+import { FETCH_STATES } from "./productReducer";
+
 export const SET_ROLES = "SET_ROLES";
 export const SET_CATEGORIES = "SET_CATEGORIES";
+export const SET_CAT_FETCH_STATE = "SET_CAT_FETCH_STATE";
 export const SET_THEME = "SET_THEME";
 export const SET_LANGUAGE = "SET_LANGUAGE";
 
 const initialState = {
   roles: [],
   categories: [],
+  catFetchState: FETCH_STATES.NOT_FETCHED,
   theme: "",
   language: "",
 };
@@ -21,6 +25,11 @@ export const globalReducer = (state = initialState, action) => {
       return {
         ...state,
         categories: action.payload,
+      };
+    case SET_CAT_FETCH_STATE:
+      return {
+        ...state,
+        catFetchState: action.payload,
       };
     case SET_THEME:
       return {
