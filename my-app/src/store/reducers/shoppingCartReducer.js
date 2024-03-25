59b062ed2e59ce4171b175d11135cbd1a3627472
Aytekinozis <aytekinozis93@gmail.com>
2024-03-25 16:05:46 +0300

    cart reducer update

--- my-app/src/store/reducers/shoppingCartReducer.js
+++ my-app/src/store/reducers/shoppingCartReducer.js
@@ -36,7 +36,7 @@ export const shoppingCartReducer = (state = initialState, action) => {
     case SET_CART:
       return {
         ...state,
-        cart: action.payload,
+        cart: [...state.cart, action.payload],
       };
     case SET_PAYMENT:
       return {
