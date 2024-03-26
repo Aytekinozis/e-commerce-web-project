8d6c98cd7dd7410e441ba191ed65f82981ea0352
Aytekinozis <aytekinozis93@gmail.com>
2024-03-26 15:23:50 +0300

    count down added

--- my-app/src/store/reducers/shoppingCartReducer.js
+++ my-app/src/store/reducers/shoppingCartReducer.js
@@ -9,6 +9,7 @@ export const SET_PAYMENT = "SET_PAYMENT";
 export const SET_ADDRESS = "SET_ADDRESS";
 export const REMOVE_PRODUCT = "REMOVE_PRODUCT";
 export const SET_CHECKED = "SET_CHECKED";
+export const SET_DOWN_COUNT = "SET_DOWN_COUNT";
 
 export const shoppingCartReducer = (state = initialState, action) => {
   switch (action.type) {
@@ -34,10 +35,6 @@ export const shoppingCartReducer = (state = initialState, action) => {
         };
       }
     case REMOVE_PRODUCT:
-      const removedProductIndex = state.cart.findIndex(
-        (product) => product.product.id === action.payload.id
-      );
-
       const remainingCart = state.cart.filter(
         (product) => product.product.id !== action.payload
       );
@@ -55,6 +52,18 @@ export const shoppingCartReducer = (state = initialState, action) => {
       return {
         ...state,
         cart: updatedCart,
+      };
+
+    case SET_DOWN_COUNT:
+      const downIndex = state.cart.findIndex(
+        (product) => product.product.id === action.payload
+      );
+
+      const newCart = [...state.cart];
+      newCart[downIndex].count--;
+      return {
+        ...state,
+        cart: newCart,
       };
 
     case SET_PAYMENT:
