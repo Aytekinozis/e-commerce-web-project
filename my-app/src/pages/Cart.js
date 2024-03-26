1d25ae16bec9f88a636d3483493ec404b75490b4
Aytekinozis <aytekinozis93@gmail.com>
2024-03-26 18:11:31 +0300

    order summary added

--- my-app/src/pages/Cart.js
+++ my-app/src/pages/Cart.js
@@ -16,32 +16,41 @@ const Cart = () => {
   let totalCart = shoppingCart.reduce((total, item) => {
     return total + item.count;
   }, 0);
+
+  const deliveryFee = total > 300 ? "Free" : "$15";
   return (
     <>
       <Header />
       <div className="flex flex-col items-center">
         <h3 className="text-3xl font-bold my-4">My Cart</h3>
-        <div>
-          {shoppingCart.length > 0 ? (
-            <>
-              <List className="p-0 border-b border-blue-gray-50">
-                {shoppingCart.map((product) => (
-                  <ListItem className="shadow" ripple={false}>
-                    <CartProductLg product={product}></CartProductLg>
-                  </ListItem>
-                ))}
-              </List>
-              <Button
-                onClick={() => history.push("/cart")}
-                className="mt-4 ml-12"
-                color="blue"
-              >
-                Confirm Purchase
-              </Button>
-            </>
-          ) : (
-            <p>Shopping Cart Is Empty</p>
-          )}
+        <div className="flex gap-8 min-h-[500px]">
+          <div className="my-4">
+            {shoppingCart.length > 0 ? (
+              <>
+                <List className="p-0 border-b border-blue-gray-50">
+                  {shoppingCart.map((product) => (
+                    <ListItem className="shadow" ripple={false}>
+                      <CartProductLg product={product}></CartProductLg>
+                    </ListItem>
+                  ))}
+                </List>
+              </>
+            ) : (
+              <p>Shopping Cart Is Empty</p>
+            )}
+          </div>
+          <div className="flex flex-col items-center gap-4 w-3/12 max-w-96 max-h-96 rounded-lg shadow p-4">
+            <p className="text-lg font-bold">Order Summary</p>
+            <div className="flex w-full justify-between px-4">
+              <p>Subtotal:</p>
+              <p>${total.toFixed(2)}</p>
+            </div>
+            <div className="flex w-full justify-between px-4">
+              <p>Delivery: </p>
+              <p>{deliveryFee}</p>
+            </div>
+            <Button color="blue">Create Order</Button>
+          </div>
         </div>
       </div>
       <Footer />
