2e326607a8e8ac5296ba6c122c9eb935bc08e282
Aytekinozis <aytekinozis93@gmail.com>
2024-03-28 18:33:31 +0300

    order page summary added

--- my-app/src/pages/Order.js
+++ my-app/src/pages/Order.js
@@ -1,11 +1,49 @@
+import { Button } from "@material-tailwind/react";
 import Footer from "../layout/Footer";
 import Header from "../layout/Header";
+import { useSelector } from "react-redux";
 
 const Order = () => {
+  const shoppingCart = useSelector((store) => store.shoppingCart.cart);
+
+  const total = shoppingCart.reduce((total, item) => {
+    if (item.checked) {
+      return total + item.count * item.product.price;
+    } else {
+      return total + 0;
+    }
+  }, 0);
+
+  const totalDeliveryFee = shoppingCart.reduce((total, item) => {
+    if (item.checked && item.count * item.product.price < 300) {
+      return total + 15;
+    } else {
+      return total + 0;
+    }
+  }, 0);
+
+  const deliveryFee = totalDeliveryFee === 0 ? "Free" : "$" + totalDeliveryFee;
   return (
     <>
       <Header />
-      <div></div>
+      <div>
+        <div className="flex flex-col items-center gap-4 w-3/12 max-w-96 max-h-80 rounded-lg shadow p-4">
+          <p className="text-lg font-bold">Order Summary</p>
+          <div className="flex w-full justify-between px-4">
+            <p>Subtotal:</p>
+            <p>${total.toFixed(2)}</p>
+          </div>
+          <div className="flex w-full pb-4 justify-between px-4 border-b border-blue-gray-200">
+            <p>Delivery Fee: </p>
+            <p>{deliveryFee}</p>
+          </div>
+          <div className="flex w-full pb-4 justify-between px-4">
+            <p>Total: </p>
+            <p>${(total + totalDeliveryFee).toFixed(2)}</p>
+          </div>
+          <Button color="blue">Create Order</Button>
+        </div>
+      </div>
       <Footer />
     </>
   );
