4d625c9cc2dd9223db5d00414780a64e4e7256ae
Aytekinozis <aytekinozis93@gmail.com>
2024-03-28 18:21:04 +0300

    order page created

---
+++ my-app/src/pages/Order.js
@@ -0,0 +1,14 @@
+import Footer from "../layout/Footer";
+import Header from "../layout/Header";
+
+const Order = () => {
+  return (
+    <>
+      <Header />
+      <div></div>
+      <Footer />
+    </>
+  );
+};
+
+export default Order;
