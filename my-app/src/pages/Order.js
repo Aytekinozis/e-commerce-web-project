db5a9ce8b37af9ec8ce25e007e2a89066d63e5f4
Aytekinozis <aytekinozis93@gmail.com>
2024-04-02 16:49:35 +0300

    adress and payment init

--- my-app/src/pages/Order.js
+++ my-app/src/pages/Order.js
@@ -1,4 +1,11 @@
-import { Button } from "@material-tailwind/react";
+import {
+  Button,
+  Tab,
+  TabPanel,
+  Tabs,
+  TabsBody,
+  TabsHeader,
+} from "@material-tailwind/react";
 import Footer from "../layout/Footer";
 import Header from "../layout/Header";
 import { useSelector } from "react-redux";
@@ -23,10 +30,46 @@ const Order = () => {
   }, 0);
 
   const deliveryFee = totalDeliveryFee === 0 ? "Free" : "$" + totalDeliveryFee;
+
+  const data = [
+    {
+      label: "Address Information",
+      value: "Address Information",
+      desc: `It really matters and then like it really doesn't matter.
+      What matters is the people who are sparked by it. And the people 
+      who are like offended by it, it doesn't matter.`,
+    },
+    {
+      label: "Payment Options",
+      value: "Payment Options",
+      desc: `Because it's about motivating the doers. Because I'm here
+      to follow my dreams and inspire other people to follow their dreams, too.`,
+    },
+  ];
   return (
     <>
       <Header />
-      <div>
+
+      <div className="flex gap-4 justify-center">
+        <div className="w-1/2">
+          <Tabs value="Address Information">
+            <TabsHeader>
+              {data.map(({ label, value }) => (
+                <Tab key={value} value={value}>
+                  {label}
+                </Tab>
+              ))}
+            </TabsHeader>
+            <TabsBody>
+              {data.map(({ value, desc }) => (
+                <TabPanel key={value} value={value}>
+                  {desc}
+                </TabPanel>
+              ))}
+            </TabsBody>
+          </Tabs>
+        </div>
+
         <div className="flex flex-col items-center gap-4 w-3/12 max-w-96 max-h-80 rounded-lg shadow p-4">
           <p className="text-lg font-bold">Order Summary</p>
           <div className="flex w-full justify-between px-4">
