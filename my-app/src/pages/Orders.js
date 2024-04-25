fddc0e1bc883211dae840af6fd67e40abd6be501
Aytekinozis <aytekinozis93@gmail.com>
2024-04-25 19:26:20 +0300

    accordion added

--- my-app/src/pages/Orders.js
+++ my-app/src/pages/Orders.js
@@ -3,9 +3,16 @@ import Footer from "../layout/Footer";
 import Header from "../layout/Header";
 import { API } from "../api/api";
 import { toast } from "react-toastify";
+import {
+  Accordion,
+  AccordionBody,
+  AccordionHeader,
+} from "@material-tailwind/react";
 
 const Orders = () => {
   const [orders, setOrders] = useState([]);
+  const [open, setOpen] = useState(1);
+  const handleOpen = (value) => setOpen(open === value ? 0 : value);
 
   useEffect(() => {
     API.get("/order")
@@ -21,20 +28,47 @@ const Orders = () => {
   return (
     <>
       <Header />
+      <h3 className="font-bold text-4xl text-center underline my-4">
+        Previous Orders
+      </h3>
+      <div className=" flex flex-col items-center">
+        <div className=" w-3/5">
+          {orders.length > 0 ? (
+            <>
+              {orders.map((item) => (
+                <Accordion open={open === item.id}>
+                  <AccordionHeader onClick={() => handleOpen(item.id)}>
+                    Order ID:{item.id}
+                  </AccordionHeader>
+                  <AccordionBody>
+                    {item.products.length > 0 ? (
+                      <>
+                        {item.products.map((product) => (
+                          <div>
+                            <p>{product.name}</p>
+                            <p>{product.price}</p>
+                          </div>
+                        ))}
+                        <div></div>
+                        <p>Toplam: {item.price}</p>
+                      </>
+                    ) : (
+                      <></>
+                    )}
+                  </AccordionBody>
+                </Accordion>
+              ))}
+            </>
+          ) : (
+            <>
+              <h3 className="text-center text-4xl font-bold my-32">
+                You Have No Previous Orders
+              </h3>
+            </>
+          )}
+        </div>
+      </div>
 
-      {orders.length > 0 ? (
-        <>
-          {orders.map((item) => {
-            "urun";
-          })}
-        </>
-      ) : (
-        <>
-          <h3 className="text-center text-4xl font-bold my-32">
-            You Have No Previous Orders
-          </h3>
-        </>
-      )}
       <Footer />
     </>
   );
